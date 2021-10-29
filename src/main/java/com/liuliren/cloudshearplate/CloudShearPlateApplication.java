da1bd78a48fa1fb26ff3e8f3349447059c46d5ca
刘耀文 <lyw001@nit.edu.cn>
2021-10-29 20:35:22 +0800

    SpringBoot启动类添加注释

--- src/main/java/com/liuliren/cloudshearplate/CloudShearPlateApplication.java
+++ src/main/java/com/liuliren/cloudshearplate/CloudShearPlateApplication.java
@@ -3,6 +3,10 @@ package com.liuliren.cloudshearplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author LiuYaoWen
+ * @date 2021/10/29 下午8:34
+ */
 @SpringBootApplication
 public class CloudShearPlateApplication {
 
