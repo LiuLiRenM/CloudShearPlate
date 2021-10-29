a291d3058ea0505bbfe1aa43d5748848e67f64d5
刘耀文 <lyw001@nit.edu.cn>
2021-10-29 20:30:30 +0800

    项目初始化

---
+++ src/main/java/com/liuliren/cloudshearplate/CloudShearPlateApplication.java
@@ -0,0 +1,13 @@
+package com.liuliren.cloudshearplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudShearPlateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudShearPlateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liuliren/cloudshearplate/CloudShearPlateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuliren.cloudshearplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudShearPlateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
